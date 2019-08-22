2eaade25c7e7a317076d869e3251569eb430b980
samoojj <sam.moore@pathai.com>
2019-08-22 16:22:32 -0400

    Default exports for strings

--- src/strings.js
+++ src/strings.js
@@ -3,3 +3,11 @@ export const errored = slug => `errored_${slug}`;
 export const requested = slug => `requested_${slug}`;
 export const received = slug => `received_${slug}`;
 export const failed = slug => `failed_${slug}`;
+
+export default {
+  loaded,
+  errored,
+  requested,
+  received,
+  failed
+};
