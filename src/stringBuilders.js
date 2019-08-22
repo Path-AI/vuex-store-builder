b881cc05efb0a68468fb70ef5a67f56da9dbf1e9
samoojj <sam.moore@pathai.com>
2019-08-22 09:48:14 -0400

    Initial utilities

---
+++ src/stringBuilders.js
@@ -0,0 +1,5 @@
+export const loaded = slug => `loaded_${slug}`;
+export const errored = slug => `errored_${slug}`;
+export const requested = slug => `requested_${slug}`;
+export const received = slug => `received_${slug}`;
+export const failed = slug => `failed_${slug}`;
