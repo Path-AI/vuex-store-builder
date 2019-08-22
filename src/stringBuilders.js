8110fb198b7d79486d94e23bbfdbacc1194e055e
samoojj <sam.moore@pathai.com>
2019-08-22 16:02:48 -0400

    Deleted file was restored in previous merge

--- src/stringBuilders.js
+++
@@ -1,5 +0,0 @@
-export const loaded = slug => `loaded_${slug}`;
-export const errored = slug => `errored_${slug}`;
-export const requested = slug => `requested_${slug}`;
-export const received = slug => `received_${slug}`;
-export const failed = slug => `failed_${slug}`;
