9652326c1bd19340ab13f0c04521af346acb2ac8
samoojj <sam.moore@pathai.com>
2019-08-22 18:31:27 -0400

    tests for vuexStoreBuilder

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,4 @@
-import { requested, received, failed } from "./strings";
+import { requested, received, failed, loaded, errored } from "./strings";
 import { requestBuilder, receiveBuilder, failBuilder } from "./mutations";
 import { defaultActionBuilder } from "./actions";
 
@@ -9,10 +9,10 @@ jest.mock("./mutations");
 jest.mock("./actions");
 
 describe("vuexStoreBuilder", () => {
+  const slug = "slug";
+  const call = jest.fn();
   afterEach(jest.clearAllMocks);
   describe("default configuration", () => {
-    const slug = "slug";
-    const call = jest.fn();
     describe("mutation", () => {
       describe("name builders", () => {
         it.each([
@@ -53,6 +53,88 @@ describe("vuexStoreBuilder", () => {
           failedMutationName
         }
       ]);
+    });
+    describe("generated store has expected shape", () => {
+      let store;
+      beforeEach(() => {
+        store = vuexStoreBuilder(slug, call);
+      });
+      it("is namespaced", () => {
+        expect(store.namespaced).toBe(true);
+      });
+      describe("state", () => {
+        loaded.mockImplementation(jest.requireActual("./strings").loaded);
+        errored.mockImplementation(jest.requireActual("./strings").errored);
+        it.each([
+          ["byId", {}],
+          [loaded(slug), false],
+          [errored(slug), undefined]
+        ])("%s has expected initial value, %o", (attribute, initial) => {
+          expect(store.state[attribute]).toEqual(initial);
+        });
+      });
+      describe("getters", () => {
+        it("lists the values in byId", () => {
+          const one = {};
+          const two = {};
+          const byId = {
+            one,
+            two
+          };
+          const state = {
+            byId
+          };
+          expect(store.getters.list(state)).toStrictEqual([one, two]);
+        });
+      });
+    });
+  });
+  describe("overrides", () => {
+    describe("mutations", () => {
+      it("spreads additional provided mutations", () => {
+        const extraMutation = jest.fn();
+        expect(
+          vuexStoreBuilder(slug, call, {
+            mutations: {
+              extraMutation
+            }
+          }).mutations.extraMutation
+        ).toBe(extraMutation);
+      });
+      it.each([
+        ["request", "requestedMutationName"],
+        ["receive", "receivedMutationName"],
+        ["fail", "failedMutationName"]
+      ])(
+        "defines provided %s mutation with provided mutation name, %s",
+        (name, mutationName) => {
+          const built = jest.fn();
+          expect(
+            vuexStoreBuilder(slug, call, {
+              [name]: built,
+              [mutationName]: mutationName
+            }).mutations[mutationName]
+          ).toBe(built);
+        }
+      );
+    });
+    describe("actions", () => {
+      it("spreads additional provided actions", () => {
+        const action2 = jest.fn();
+        expect(
+          vuexStoreBuilder(slug, call, {
+            actions: {
+              action2
+            }
+          }).actions.action2
+        ).toBe(action2);
+      });
+      it("defines provided action with key slug", () => {
+        const action = jest.fn();
+        expect(vuexStoreBuilder(slug, call, { action }).actions[slug]).toBe(
+          action
+        );
+      });
     });
   });
 });
