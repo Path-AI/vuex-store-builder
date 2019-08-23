c571134b41f7c47f8ef094b7f4d0b5eef831f7ba
Samuel Moore <samueljosiahmoore@gmail.com>
2019-08-22 20:25:28 -0400

    Mutations spec (#7)
    * Tests for requestBuilder
    
    * More coverage for mutations

---
+++ src/mutations.spec.js
@@ -0,0 +1,61 @@
+import { loaded, errored } from "./strings";
+import { requestBuilder, receiveBuilder, failBuilder } from "./mutations";
+
+jest.mock("./strings");
+
+describe("mutation builders", () => {
+  const slug = "slug";
+  describe("requestBuilder", () => {
+    it.each([
+      ["loaded on state", loaded, false],
+      ["errored on state", errored, {}]
+    ])("requestBuilder puts %s", (mockedValue, attributeBuilder, initial) => {
+      attributeBuilder.mockImplementationOnce(() => mockedValue);
+      const state = {};
+      requestBuilder(slug)(state);
+      expect(state[mockedValue]).toEqual(initial);
+    });
+  });
+  describe("receiveBuilder", () => {
+    const getKey = jest.fn(({ id }) => id);
+    it("handles data arrays", () => {
+      const datum1 = { id: 123 };
+      const datum2 = { id: 234 };
+      const state = {
+        byId: {}
+      };
+      receiveBuilder(slug, getKey)(state, [datum1, datum2]);
+      expect(state.byId).toEqual({
+        123: datum1,
+        234: datum2
+      });
+    });
+    it("handles data singletons", () => {
+      const datum = { id: 123 };
+      const state = {
+        byId: {}
+      };
+      receiveBuilder(slug, getKey)(state, datum);
+      expect(state.byId).toEqual({
+        123: datum
+      });
+    });
+    it("flips loaded boolean", () => {
+      loaded.mockImplementationOnce(() => "loaded");
+      const state = {
+        byId: {}
+      };
+      receiveBuilder(slug, getKey)(state, {});
+      expect(state.loaded).toBe(true);
+    });
+  });
+  describe("failBuilder", () => {
+    it("passes error into state", () => {
+      errored.mockImplementationOnce(() => "errored");
+      const state = {};
+      const error = {};
+      failBuilder(slug)(state, error);
+      expect(state.errored).toBe(error);
+    });
+  });
+});
